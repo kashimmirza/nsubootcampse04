dab81e680373ef668eb496f233f660e62140ff28
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-05-13 02:09:43 +0600

    bootcamp04.cpp

---
+++ addsum.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,a,b,sum;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        cin>>a>>b;
+        sum=a+b;
+        cout<<"Case"<<" "<<i<<":"<<" "<<sum<<endl;
+
+
+
+    }
+
+
+
+
+
+}
